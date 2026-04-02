38bcb423ec12884d570593e22d864e3e4d1d75fe
DmytroPaliichuk <dmitry.paliychuk@gmail.com>
2026-04-01 22:58:47 -0700

    add chat

--- frontend-next/app/page.tsx
+++ frontend-next/app/page.tsx
@@ -1,3 +1,4 @@
+import Chat from '@/components/Chat'
 import UsMap from '@/components/UsMap'
 import citiesData from '@/public/cities.json'
 
@@ -5,11 +6,14 @@ export default function Home() {
   const cities = citiesData.hometown
 
   return (
-    <main className="flex flex-col items-center px-4 py-6">
-      <h1 className="text-2xl font-semibold tracking-wide text-slate-100 mb-5">
-        Hometown Map
-      </h1>
-      <UsMap cities={cities} />
+    <main className="flex flex-row h-screen overflow-hidden">
+      <section className="flex flex-col items-center flex-1 px-4 py-6 overflow-y-auto">
+        <h1 className="text-2xl font-semibold tracking-wide text-slate-100 mb-5">
+          Hometown Map
+        </h1>
+        <UsMap cities={cities} />
+      </section>
+      <Chat />
     </main>
   )
 }
